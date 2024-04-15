e7fa71d84d94db5aa6fc2ef98a3ed918dafb03a8
Vishnu P S <vishnu13three@gamil.com>
2024-04-15 18:58:46 +0530

    first api and handeling GET  request

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 //IMPORT PACKAGE
 const express = require ('express');
+const fs = require('fs');
+
 const app = express();
 
+let movies = JSON.parse(fs.readFileSync('./data/movies.json'));
+
+
 //ROUTE = HTTP METHOD + URL
-app.get('/',(req,res) =>{
+// app.get('/',(req,res) =>{
     // res.status(200).send('Hello from server');
     // res.status(200).send('<h4>Hello from server</h4>');
-    res.status(200).json({message:"hello world",status:200});
+    // res.status(200).json({message:"hello world",status:200});
+// });
 
+
+//GET - api/movies
+app.get('/api/movies',(req,res) => {
+    res.status(200).json({
+        status: "success",
+        data : {
+            movies : movies
+        }
+    })
 });
 
 //CREATE SERVER
